4553966f5a2807332f6a5b570fa2967c5597d28a
Daniel Edgecombe <edgy.edgemond@gmail.com>
2024-09-09 16:54:48 +0100

    fix lint

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -25,7 +25,7 @@ class CustomValidationError(error.StatusProblem):
     title = "Request validation error."
 
 
-@pytest.fixture()
+@pytest.fixture
 def cors():
     return CorsConfiguration(
         allow_origins=["*"],
