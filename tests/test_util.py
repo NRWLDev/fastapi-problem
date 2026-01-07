c2e2482e286d430a0ab586cec0e4f9f919848f39
Daniel Edgy Edgecombe <daniel@nrwl.co>
2026-01-07 17:33:52 +0000

    chore: Add latest python versions to test matrix (#47)

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from fastapi_problem import util
@@ -14,5 +16,22 @@ from fastapi_problem import util
         (422, "Unprocessable Entity", "http-unprocessable-entity"),
     ],
 )
+@pytest.mark.skipif(sys.version_info >= (3, 13), reason="python version too new")
+def test_convert_status_code_legacy(status_code, title, code):
+    assert util.convert_status_code(status_code) == (title, code)
+
+
+@pytest.mark.parametrize(
+    ("status_code", "title", "code"),
+    [
+        (500, "Internal Server Error", "http-internal-server-error"),
+        (400, "Bad Request", "http-bad-request"),
+        (401, "Unauthorized", "http-unauthorized"),
+        (404, "Not Found", "http-not-found"),
+        (409, "Conflict", "http-conflict"),
+        (422, "Unprocessable Content", "http-unprocessable-content"),
+    ],
+)
+@pytest.mark.skipif(sys.version_info < (3, 13), reason="python version too old")
 def test_convert_status_code(status_code, title, code):
     assert util.convert_status_code(status_code) == (title, code)
